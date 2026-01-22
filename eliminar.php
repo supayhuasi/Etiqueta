c1e5dbfb0cb5ba2c50026f8d9fc390e4fddcbd04
Rodrigo <coriarodrigoc@gmail.com>
2026-01-22 12:59:05 -0300

    eliminar

--- eliminar.php
+++ eliminar.php
@@ -2,6 +2,7 @@
 require 'config.php';
 
 $id = $_GET['id'];
+$pdo->prepare("DELETE FROM historial_estados WHERE productoid=?")->execute([$id]);
 $pdo->prepare("DELETE FROM productos WHERE id=?")->execute([$id]);
 
 header("Location: index.php");
