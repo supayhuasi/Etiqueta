03b774c91866413d968ff148af9f4c6b3a4dee92
Roco <coriarodrigoc@gmail.com>
2026-02-06 15:03:46 -0300

    .

--- setup_ecommerce.php
+++ setup_ecommerce.php
@@ -1,4 +1,8 @@
 <?php
 // Wrapper para ejecutar el setup del ecommerce desde la raíz
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 chdir(__DIR__ . '/ecommerce');
 require __DIR__ . '/ecommerce/setup_ecommerce.php';
