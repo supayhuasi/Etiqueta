545878348426932e6e4b1fbee586e83dd2417787
Roco <coriarodrigoc@gmail.com>
2026-03-08 17:17:42 -0300

    inventario reporte

--- admin/inventario_reporte_reponer.php
+++ admin/inventario_reporte_reponer.php
@@ -1,6 +1,11 @@
 <?php
 // Puente de compatibilidad para URL antigua /admin/inventario_reporte_reponer.php
-$query = $_SERVER['QUERY_STRING'] ?? '';
-$target = '/ecommerce/admin/inventario_reporte_reponer.php' . ($query ? ('?' . $query) : '');
-header('Location: ' . $target, true, 302);
-exit;
+$target_file = dirname(__DIR__) . '/ecommerce/admin/inventario_reporte_reponer.php';
+
+if (!is_file($target_file)) {
+	http_response_code(404);
+	echo 'No se encontró el módulo de inventario (reporte reponer).';
+	exit;
+}
+
+require $target_file;
