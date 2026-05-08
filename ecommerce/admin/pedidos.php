a001b5d12c4814e866b492210b8b176451a2de73
Rodrigo <coriarodrigoc@gmail.com>
2026-05-08 11:12:29 -0300

    pedidos

--- ecommerce/admin/pedidos.php
+++ ecommerce/admin/pedidos.php
@@ -1,6 +1,9 @@
-echo "INICIO\n";
+<?php
+echo "DEBUG SOLO PHP";
+?>
 // --- LOG DE ERRORES PERSONALIZADO ---
 ini_set('display_errors', 0);
+ini_set('log_errors', 1);
 ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/../../logs/pedidos_error.log');
 
